83c2a6f8b514ba9f8adceda7688076d65111a8da
bahoo <jon.c.culver@gmail.com>
2016-10-28 11:51:45 -0700

    miss.

--- hydra/urls.py
+++ hydra/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .views import IndexView
 
 urlpatterns = [
+    url(r'^$', IndexView.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
