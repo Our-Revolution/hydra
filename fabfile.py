73d2b32ae463f0f18706c0793e7bfc4204ddeb50
bahoo <jon.c.culver@gmail.com>
2016-10-28 11:39:46 -0700

    Really seems suboptimal but it works, sooooo [shruggie]

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,16 @@ from fabric.api import *
 def production():
     # todo: make env / variable
     env.hosts = ['ec2-35-161-201-218.us-west-2.compute.amazonaws.com']
+    env.forward_agent = True
+    env.key_filename = '~/.ssh/hydra.pem'
+    env.user = 'ubuntu'
 
 
 def deploy():
-    with cd('hydra')
-        run('workon hydra')
-        run('supervisorctl stop gunicorn')
-        run('git pull origin master')
-        run('supervisorctl start gunicorn')
-        # todo: varnish, etc.
+    with cd('hydra'):
+        with prefix('source $(which virtualenvwrapper.sh)'):
+            with prefix('workon hydra'):
+                run('supervisorctl stop gunicorn')
+                run('git pull origin master')
+                run('supervisorctl start gunicorn')
+            # todo: varnish, etc.
