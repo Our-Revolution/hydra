c9ea9f0ace58ba1801e9187e20cda9b463c169f5
Jon Culver <jon.c.culver@gmail.com>
2016-10-31 05:38:23 +0000

    Easy does it.

--- hydra/settings.py
+++ hydra/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
