d0bc3e8e47ab15d2490e521d09e2a6011a28eb9b
bahoo <jon.c.culver@gmail.com>
2016-10-31 21:18:07 -0700

    One more try, shoot.

--- hydra/urls.py
+++ hydra/urls.py
@@ -1,15 +1,19 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 import debug_toolbar
 
 urlpatterns = [
-    url(r'^__debug__/', include(debug_toolbar.urls)),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'', include('bsd.urls')),
     url('^', include('django.contrib.auth.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns.insert(0, url(r'^__debug__/', include(debug_toolbar.urls)))
 
 
 admin.site.site_header = "Hydra administration"
