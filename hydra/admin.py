ee6da38f8f3abcf39b85d20cd3f77ee042b747e5
bahoo <jon.c.culver@gmail.com>
2016-11-02 15:32:03 -0700

    Here we go.

--- hydra/admin.py
+++ hydra/admin.py
@@ -9,12 +9,6 @@ from bsd.auth import Constituent
 
 class EventPromotionRequestAdminForm(forms.ModelForm):
     send_preview_email_to = forms.CharField(max_length=1024, help_text="Comma separated email addresses -- filling this out will ONLY send a preview.", required=False)
-
-    def save_model(self, request, obj, form, change):
-        save_kwargs = {}
-        if form.get('send_preview_email_to', False):
-            save_kwargs['preview'] = form['send_preview_email_to']
-        obj.save(**save_kwargs)
     
     class Meta:
         model = EventPromotionRequest
@@ -30,6 +24,12 @@ class EventPromotionRequestAdmin(admin.ModelAdmin):
     list_display = ['event_name', 'event_type', 'host_name', 'submitted', 'status']
     raw_id_fields = ['event', 'host', 'recipients']
     form = EventPromotionRequestAdminForm
+
+    def save_model(self, request, obj, form, change):
+        save_kwargs = {}
+        if form.cleaned_data.get('send_preview_email_to', False):
+            save_kwargs['preview'] = form.cleaned_data['send_preview_email_to']
+        obj.save(**save_kwargs)
     
     def get_object(self, request, object_id, from_field=None):
         obj = super(EventPromotionRequestAdmin, self).get_object(request, object_id)
