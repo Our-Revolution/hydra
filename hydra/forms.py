f8d9ff29166c5301adc6acb63623e172e873883e
Chris Mabry <cjmab28@gmail.com>
2017-10-24 19:02:34 -0500

    char limit to 1 million for cincy :|

--- hydra/forms.py
+++ hydra/forms.py
@@ -15,7 +15,7 @@ class BlastEmailForm(forms.Form):
 
 class GeoTargetForm(forms.Form):
     state = forms.CharField(initial="FL", max_length=2, label="State Abbreviation")
-    geojson = forms.CharField(max_length=500000, widget=admin.widgets.AdminTextareaWidget, label="GeoJSON", help_text="You'll need to fetch this from Census.gov or Google or some such; ping Juliana or Jon. :D")
+    geojson = forms.CharField(max_length=1000000, widget=admin.widgets.AdminTextareaWidget, label="GeoJSON", help_text="You'll need to fetch this from Census.gov or Google or some such; ping Juliana or Jon. :D")
     primary_only = forms.BooleanField(initial=True, label="Primary Addresses Only", help_text="Recommended")
 
     def clean_geojson(self):
