23b10b428cd1c571c11e83f3b50740b21302d7e9
Chris Mabry <cjmab28@gmail.com>
2017-11-01 18:17:14 -0500

    remove commented block

--- hydra/views.py
+++ hydra/views.py
@@ -23,32 +23,6 @@ class IndexView(TemplateView):
             return redirect('events-list')
         return super(IndexView, self).get(request, *args, **kwargs)
 
-
-# @staff_member_required
-# def geotarget(request):
-#     if request.method == 'POST':
-#         post_text = request.POST.get('the_post')
-#         response_data = {}
-#
-#         post = Post(text=post_text, author=request.user)
-#         post.save()
-#
-#         response_data['result'] = 'Create post successful!'
-#         response_data['postpk'] = post.pk
-#         response_data['text'] = post.text
-#         response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
-#         response_data['author'] = post.author.username
-#
-#         return HttpResponse(
-#             json.dumps(response_data),
-#             content_type="application/json"
-#         )
-#     else:
-#         return HttpResponse(
-#             json.dumps({"nothing to see": "this isn't happening"}),
-#             content_type="application/json"
-#         )
-
 @class_view_decorator(staff_member_required)
 class GeoTarget(FormView):
     form_class = GeoTargetForm
