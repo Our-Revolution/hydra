d514414bae6915fd39a5bce1a76f9ac25909dec8
Eric Broder <eric@ourrevolution.com>
2018-06-12 14:48:47 -0700

    TECH-1072 add tech-team to debug emails

--- bsd/backends.py
+++ bsd/backends.py
@@ -9,12 +9,15 @@ class BSDAuthenticationBackend(object):
         return Constituent.objects.get(cons_id=cons_id)
 
     def authenticate(self, username, password):
-        
+
         if not '@' in username:
             return None
-        
+
         try:
+
+            """TODO: dont throw 500 error for multiple results"""
             constituent_email = ConstituentEmail.objects.get(email__iexact=username)
+
             constituent = constituent_email.cons
 
             if constituent.check_password(password):
