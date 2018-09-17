97b2f8e2f7aa3b866fd59bcbf9d9a06878e60cff
Eric Broder <eric@ourrevolution.com>
2018-09-17 10:44:47 -0700

    TECH-1182 redirect to organizing hub

--- bsd/urls.py
+++ bsd/urls.py
@@ -1,12 +1,31 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
 from . import views
 
+REDIRECT_EVENTS_URL = settings.REDIRECT_EVENTS_URL
 
 urlpatterns = [
     url(r'^events?/', include([
-        url(r'^$', views.EventsView.as_view(), name="events-list"),
-        url(r'^(?P<pk>[0-9]+)/edit$', views.EventEdit.as_view(), name="event-edit"),
-        url(r'^(?P<pk>[0-9]+)/promote$', views.EventPromote.as_view(), name="event-promote"),
-        url(r'^create$', views.EventCreate.as_view(), name="event-create"),
+        url(
+            r'^$',
+            RedirectView.as_view(url=REDIRECT_EVENTS_URL),
+            name="events-list"
+        ),
+        url(
+            r'^(?P<pk>[0-9]+)/edit$',
+            RedirectView.as_view(url=REDIRECT_EVENTS_URL),
+            name="event-edit"
+        ),
+        url(
+            r'^(?P<pk>[0-9]+)/promote$',
+            RedirectView.as_view(url=REDIRECT_EVENTS_URL),
+            name="event-promote"
+        ),
+        url(
+            r'^create$',
+            RedirectView.as_view(url=REDIRECT_EVENTS_URL),
+            name="event-create"
+        ),
     ]))
 ]
